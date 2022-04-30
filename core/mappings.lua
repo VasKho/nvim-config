00cc3fe9533cce4618a296bda018782562abfe51
VasKho <vasya.khoroshavin@gmail.com>
2022-04-30 11:54:09 +0300

    Remove unused files

--- core/mappings.lua
+++ core/mappings.lua
@@ -27,6 +27,7 @@ map("n", "<leader>f", function() require("telescope.builtin").find_files() end)
 map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration of current symbol" })
 map("n", "gd", vim.lsp.buf.definition, { desc = "Show the definition of current symbol" })
 map("n", "gI", vim.lsp.buf.implementation, { desc = "Go to implementation of current symbol" })
+map("n", "<ESC>", ":noh<CR>")
 
 
 -- Toggle comment
