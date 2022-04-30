57ba0d40c810502ee40cefc1662108546a8a4673
VasKho <vasya.khoroshavin@gmail.com>
2022-04-30 11:34:05 +0300

    Add more mappings

--- core/mappings.lua
+++ core/mappings.lua
@@ -1,14 +1,33 @@
 local map = vim.keymap.set
--- Set key bindings
+
+-- Save
 map("n", "<C-s>", ":w<CR>")
+
+
+-- Buffer navigation
 map("n", "<TAB>", ":BufferLineCycleNext<CR>")
 map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
+
+
+-- Toggle terminal
 map("n", "<leader>w", ":ToggleTerm<CR>")
+
+
+-- Toggle Neotree
 map("n", "<leader>e", ":Neotree toggle<CR>")
+
+
+-- Telescope
 map("n", "<leader>t", ":Telescope<CR>")
+map("n", "<leader>s", function() require("telescope.builtin").live_grep() end)
+map("n", "<leader>f", function() require("telescope.builtin").find_files() end)
+
+
+-- Lsp
 map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration of current symbol" })
 map("n", "gd", vim.lsp.buf.definition, { desc = "Show the definition of current symbol" })
 map("n", "gI", vim.lsp.buf.implementation, { desc = "Go to implementation of current symbol" })
-map("n", "<leader>/", function()
-  require("Comment.api").toggle_current_linewise()
-end, { desc = "Toggle comment line" })
+
+
+-- Toggle comment
+map("n", "<leader>/", function() require("Comment.api").toggle_current_linewise() end)
